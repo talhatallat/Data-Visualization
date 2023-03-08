dda51ae3d714f7d294d9b076115b3c0fdcea8909
Talha Tallat <talhatallat0@gmail.com>
2023-03-08 09:06:55 +0000

    Create Plotting a Simple Line Graph.py

---
+++ Generating Data/Plotting a Simple Line Graph.py
@@ -0,0 +1,6 @@
+#  plot a simple line graph using matplotlib and then customize it to create a more informative visualization of our data.
+import matplotlib.pyplot as plt
+
+squares = [1, 4, 9, 16, 25] #  the square number sequence 1, 4, 9, 16, 25 as the data for the graph
+plt.plot(squares)
+plt.show()
